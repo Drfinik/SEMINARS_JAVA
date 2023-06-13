d1231767d8599b6df35ae51181b60b347109c631
Drfinik <freelanceap@yahoo.com>
2023-06-13 20:01:20 +0300

    modified App.java

--- homeworks/homework_2/src/App.java
+++ homeworks/homework_2/src/App.java
@@ -1,7 +1,7 @@
 
 public class App {
-    public static void main(String[] args) throws Exception {
-       
+    public static void main(String[] args) throws Exception{
+       System.out.println("Hello Sensei");
         
     }
 }
